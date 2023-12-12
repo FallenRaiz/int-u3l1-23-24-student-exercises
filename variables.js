e37ebefeaa364f912d2c4f5f121ad5052cce27b3
FallenRaiz <110204164+FallenRaiz@users.noreply.github.com>
2023-12-12 20:58:09 +0000

    ME DID WORK :) (learned JavaScript variables and how to input strings  and values into them)

--- variables.js
+++ variables.js
@@ -1,18 +1,24 @@
 // CODE ALONG, PART 1
 let favoriteColor;
 
-// 1. Create a variable called `favoriteFood`.
 
+// 1. Create a variable called `favoriteFood`.
+let favoriteFood;
 
 // 2. Create a variable called `favoriteNumber`.
-
+let favoriteNumber;
 
 
 
 // CODE ALONG, PART 2
 //  3. Assign values for each variables.
 // - Use a string value of your favorite color.
+favoriteColor = "Purple mountain majesty";
 // - Use a string value of your favorite food.
+favoriteFood = "Tamales";
 // - Use a number value of your favorite number.
-
+favoriteNumber = 3;
 // 4. Console log each variable on a new line.
+console.log(favoriteColor)
+console.log(favoriteFood)
+console.log(favoriteNumber)
